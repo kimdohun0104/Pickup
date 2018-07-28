58483e9ef8915ee56e1fd59aa09a7cb5b0359b3a
kimdohun0104 <dikolight203@gmail.com>
2018-07-28 18:14:20 +0900

    dialog_join_party layout fixed and naming changed

---
+++ app/src/main/java/com/example/dsm2018/pickup/fragment/PartyDeleteDialogFragment.java
@@ -0,0 +1,22 @@
+package com.example.dsm2018.pickup.fragment;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dsm2018.pickup.R;
+
+public class PartyDeleteDialogFragment extends DialogFragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_party_delete, container, false);
+
+        return view;
+    }
+}
