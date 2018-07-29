15b888a2f1b52b74eebcb355e7f865b5eaf1979f
kimdohun0104 <dikolight203@gmail.com>
2018-07-29 19:21:27 +0900

    activity_user_information layout

--- app/src/main/java/com/example/dsm2018/pickup/activity/UserInformationActivity.java
+++ app/src/main/java/com/example/dsm2018/pickup/activity/UserInformationActivity.java
@@ -1,15 +1,29 @@
 package com.example.dsm2018.pickup.activity;
 
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.dsm2018.pickup.R;
 
 public class UserInformationActivity extends AppCompatActivity {
 
+    ImageView profileImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_information);
+
+        profileImage = (ImageView)findViewById(R.id.profileImage);
+        profileImage.setBackground(new ShapeDrawable(new OvalShape()));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            profileImage.setClipToOutline(true);
+        }
+        profileImage.setImageResource(R.drawable.test);
+
     }
 }
