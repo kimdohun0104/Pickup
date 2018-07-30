b3e4f864eaec6e8bf510a5cf81ad098b14699e0a
kimdohun0104 <dikolight203@gmail.com>
2018-07-30 20:26:14 +0900

    갤러리로 부터 이미지 받아오기

--- app/src/main/java/com/example/dsm2018/pickup/activity/SearchStartingPointActivity.java
+++ app/src/main/java/com/example/dsm2018/pickup/activity/SearchStartingPointActivity.java
@@ -1,32 +1,49 @@
 package com.example.dsm2018.pickup.activity;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 import com.example.dsm2018.pickup.R;
+
+import java.io.IOException;
+import java.util.List;
 
 public class SearchStartingPointActivity extends AppCompatActivity {
 
     Button backButton;
     EditText inputStartingPoint;
+    RelativeLayout beforeSearch;
+    RecyclerView recyclerView;
+    Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_starting_point);
 
+        geocoder = new Geocoder(this);
+
         inputStartingPoint = (EditText)findViewById(R.id.inputStartingPoint);
         backButton = (Button)findViewById(R.id.backButton);
+        beforeSearch = (RelativeLayout)findViewById(R.id.beforeSearch);
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-
 
 
     }
--- app/src/main/java/com/example/dsm2018/pickup/dialog/ProfileImageDialog.java
+++ app/src/main/java/com/example/dsm2018/pickup/dialog/ProfileImageDialog.java
@@ -1,19 +1,37 @@
 package com.example.dsm2018.pickup.dialog;
 
+import android.Manifest;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.dsm2018.pickup.R;
+
+import java.io.IOException;
 
 public class ProfileImageDialog extends DialogFragment {
 
     Button cancelButton;
+    LinearLayout changeImage;
+    ImageView profileImage;
+    private final int REQUEST_PERMISSION_CODE = 101;
+    private final int GALLERY_CODE=1112;
 
     @Override
     public void onResume() {
@@ -28,6 +46,12 @@ public class ProfileImageDialog extends DialogFragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.dialog_profile_image, container, false);
 
+        ActivityCompat.requestPermissions(getActivity(),
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                REQUEST_PERMISSION_CODE);
+
+        profileImage = (ImageView)view.findViewById(R.id.profileImage);
+        changeImage = (LinearLayout)view.findViewById(R.id.changeImage);
         cancelButton = (Button)view.findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +60,68 @@ public class ProfileImageDialog extends DialogFragment {
             }
         });
 
+        changeImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setData(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent, GALLERY_CODE);
+            }
+        });
+
         return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        sendPicture(data.getData());
+    }
+
+    public void sendPicture(Uri imgUri){
+        String imagePath = getRealPathFromURI(imgUri); // path 경로
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(imagePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int exifOrientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+        int exifDegree = exifOrientationToDegrees(exifOrientation);
+
+        Bitmap bitmap = BitmapFactory.decodeFile(imagePath);//경로를 통해 비트맵으로 전환
+        changeImage.setVisibility(View.INVISIBLE);
+        profileImage.setImageBitmap(rotate(bitmap, exifDegree));
+    }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        int column_index=0;
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = getActivity().getContentResolver().query(contentUri, proj, null, null, null);
+        if(cursor.moveToFirst()){
+            column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        }
+
+        return cursor.getString(column_index);
+    }
+
+    private int exifOrientationToDegrees(int exifOrientation) {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180;
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270;
+        }
+        return 0;
+    }
+
+    private Bitmap rotate(Bitmap src, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
+    }
+
 }
